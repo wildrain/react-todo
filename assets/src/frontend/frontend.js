2441574e4c9295f45ac0b6e59b5b7267fa264972
roman <roman@romans-MacBook-Pro.local>
2015-12-02 16:49:05 +0600

    todoapp done

--- assets/src/frontend/frontend.js
+++ assets/src/frontend/frontend.js
@@ -1,4 +1,8 @@
-var React = require('react');
+var jquery = require('jquery'),
+	React = require('react'),
+	_lo   = require('lodash');
+
+require('jquery-ui');	
 
 var ReactTodo = React.createClass({
 
@@ -12,13 +16,20 @@ var ReactTodo = React.createClass({
 		}
 	},
 
+	componentDidMount : function(){
+		var self = this;
+		jquery('#sortable').sortable({
+			stop: self._handleSortingUpdate
+		});		
+	},
+
 	render: function(){
 		var self = this;
 		var item = function(todoItem , index){
 			var style = {
 				color : todoItem.color
 			};			
-			return <li style={style} key={index + todoItem}>
+			return <li className="sort" style={style} data-uid = {todoItem.uid} key={todoItem.uid}>
 						{todoItem.text}<button type="button" onClick={self._handleButtonSubmit.bind(this,todoItem)}> showPanel </button>
 						<button type="button" onClick={self._handleDeleteItem.bind(this,todoItem)}> deleteTodo </button>
 						{todoItem.showPanel ? <p><input type="text" id={todoItem.uid} onChange={self._handleChangeColor} /></p> : null }
@@ -29,7 +40,7 @@ var ReactTodo = React.createClass({
 			<div>
 				<h3 className="heading">ReactTodo App</h3>
 				{(this.state.items.length ? 
-					<ul>
+					<ul id="sortable">
 						{this.state.items.map(item)}
 					</ul> 
 					: 
@@ -59,6 +70,7 @@ var ReactTodo = React.createClass({
 			'showPanel': false,
 		});
 		this.setState({text: ''});
+		this._handleSavingData(allItems);
 
 		e.preventDefault();
 	},
@@ -95,14 +107,26 @@ var ReactTodo = React.createClass({
 		this._handleSavingData(this.state.items);
 	},
 
+	_handleSortingUpdate: function(event, ui){
+		var updateItems = [],
+		    itemIds = [],
+		    self = this;
+		jquery( 'ul#sortable' ).find( 'li.sort' ).each( function(index, item) {
+			itemIds.push(jquery(item).data('uid'));
+		});
+		itemIds.forEach(function(itemId){					
+			var index = _lo.findIndex(self.state.items, {uid : itemId});
+			updateItems.push(self.state.items[index]);
+		})
+		localStorage.setItem('todoLists' , JSON.stringify(updateItems));				
+	},
+
 	_handleSavingData: function(items){
 		localStorage.setItem('todoLists' , JSON.stringify(items));
 		this.setState({items: items});
 	}
 
 });
-
-
 
 React.render(<ReactTodo /> , document.getElementById('app'));
 
